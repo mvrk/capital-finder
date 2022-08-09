4642c5bf99d5ef867bb20389bfe5d1254619dbe6
Alex Guo <guojiarui@gmail.com>
2022-08-08 22:43:10 -0700

    update for vercel deployment.

--- api/message.py
+++ api/message.py
@@ -10,6 +10,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("content-type", "text/plain")
         self.end_headers()
-        message = "please input a correct name"
+        message = "please follow format 'country=name' or 'city=name', and input existed names to query."
         self.wfile.write(message.encode())
         return
